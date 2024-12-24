28d0ce6ccb4091f4263fb165d10da7ef3b45428a
Bedo <bedonassef71@gmail.com>
2024-12-24 21:45:21 +0200

    app.module > +songs module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { IamModule } from './iam/iam.module';
 import { StreamModule } from './stream/stream.module';
+import { SongsModule } from './songs/songs.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, IamModule, StreamModule],
+  imports: [UsersModule, DatabaseModule, IamModule, StreamModule, SongsModule],
 })
 export class AppModule {}
